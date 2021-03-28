3a86ecc10db8d6153daa08654071485d7026f632
upupsheep <sandy861003@gmail.com>
2021-03-28 23:38:38 +0800

    lab1

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -59,9 +59,57 @@ def derivative_sigmoid(x):
     return np.multiply(x, 1.0 - x)
 
 
+class Layer():
+    def __init__(self, input_size, output_size):
+        self.w = np.random.normal(0, 1, (input_size+1, output_size))
+
+    def forward(self, x):
+        x = np.append(x, np.ones((x.shape[0], 1)), axis=1)
+        self.forward_gradient = x
+        self.y = sigmoid(x @ self.w)
+        return self.y
+
+    def backward(self, derivative_C):
+        self.backward_gradient = derivative_sigmoid(self.y) @ derivative_C
+        return self.backward_gradient @ self.w[:-1].T
+
+    def update(self, learning_rate):
+        self.gradient = self.forward_gradient.T @ self.backward_gradient
+        self.w -= learning_rate * self.gradient
+        return self.gradient
+
+
+class NN():
+    def __init__(self, sizes, learning_rate=0.1):
+        self.learning_rate = learning_rate
+        sizes2 = sizes[1:] + [0]
+        self.l = []
+        for a, b in zip(sizes, sizes2):
+            if (a+1)*b == 0:
+                continue
+            self.l += [Layer(a, b)]
+
+    def forward(self, x):
+        _x = x
+        for l in self.l:
+            _x = l.forward(_x)
+        return _x
+
+    def backward(self, dC):
+        _dC = dC
+        for l in self.l[::-1]:
+            _dC = l.backward(_dC)
+
+    def update(self):
+        gradients = []
+        for l in self.l:
+            gradients += [l.update(self.learning_rate)]
+        return gradients
+
+
 if __name__ == '__main__':
-    x, y = generate_linear(n=100)
+    x, y = generate_linear(n=100)  # (100, 2), (100, 1)
     x = np.array(x)
     y = np.array(y)
-    print(x)
-    print(y)
+    print(x.shape)
+    print(y.shape)
